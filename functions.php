c6f874bd3dd196282b4e0fc62c4a91e7df50829f
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-09-30 10:36:23 +0300

    fn example

--- functions.php
+++ functions.php
@@ -1,5 +1,21 @@
 <?php 
 	// functions.php
 	// siia tulevad funktsioonid, kõik mis seotud AB'iga
-
+	
+	function welcomeUserOne($name) {
+		echo "Tere ".$name;
+	}
+	
+	function welcomeUserTwo($name) {
+		return "Tere ".$name;
+	}
+	
+	welcomeUserOne("Juku");
+	welcomeUserOne("Mari");
+	
+	echo welcomeUserTwo("Juhan");
+	
+	$welcome_user = welcomeUserTwo("Romil");
+	echo $welcome_user;
+	
 ?>
